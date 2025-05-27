452c83b358bf2cada3d7883a20f4d155aaa72ea6
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-05-27 17:02:11 +0200

    Shop er opsat med tilbehør

--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 import Overskrift from "../components/Overskrift";
-import Underoverskrift from "../components/Underoverskrift";
 import Beskrivelse from "../components/Beskrivelse";
+import pil from "../assets/pil.svg"; // Importerer pil-ikonet
+import frisureEt from "../assets/frisureEt.svg"; // Importerer frisure-billedet
+import frisureTo from "../assets/frisureTo.svg"; // Importerer frisure-billedet
+import frisureTre from "../assets/frisureTre.svg"; // Importerer frisure-billedet
+import ansigtEt from "../assets/ansigtEt.svg"; // Importerer ansigt-billedet
+import ansigtTo from "../assets/ansigtTo.svg"; // Importerer ansigt-billedet
+import ansigtTre from "../assets/ansigtTre.svg"; // Importerer ansigt-billedet
+import skoEt from "../assets/skoEt.svg"; // Importerer sko-billedet
+import skoTo from "../assets/skoTo.svg"; // Importerer sko-billedet
+import skoTre from "../assets/skoTre.svg"; // Importerer sko-billedet
 
 export default function Shop() {
-  const [tema, setTema] = useState(() => localStorage.getItem("tema") || "");
+  const [tema, setTema] = useState(() => sessionStorage.getItem("tema") || "");
 
   // Kør dette hver gang tema ændrer sig
   useEffect(() => {
     // Fjern gamle tema-klasser
-    document.body.classList.remove(
-      "tema-roed",
-      "tema-gul",
-      "tema-groen",
-      "tema-blaa"
-    ); // Fjerner de gamle tema-klasser fra body, så der ikke er flere temaer aktive på samme tid
+    document.body.classList.remove("tema-roed", "tema-gul", "tema-groen"); // Fjerner de gamle tema-klasser fra body, så der ikke er flere temaer aktive på samme tid
 
     // Tilføj det aktuelle tema
     document.body.classList.add(`tema-${tema}`); // Tilføjer tema-klassen til body. Klassen er i formatet tema-[farve] og tager [farve] fra state-variablen tema
 
-    // Gem i localStorage så det huskes
-    localStorage.setItem("tema", tema); // Gemmer det aktuelle tema i localStorage, så det kan hentes igen ved næste indlæsning af siden
-  }, [tema]); // useEffect kører hver gang tema ændrer sig, så den kan opdatere body-klassen og localStorage
+    // Gem i sessionStorage så det huskes
+    sessionStorage.setItem("tema", tema); // Gemmer det aktuelle tema i sessionStorage, så det kan hentes igen ved næste indlæsning af siden
+  }, [tema]); // useEffect kører hver gang tema ændrer sig, så den kan opdatere body-klassen og sessionStorage
 
   return (
     <>
@@ -31,42 +35,214 @@ export default function Shop() {
           "Velkommen til shoppen! Her kan du selv bestemme designet for din app. Optjen point ved at løse opgaver og lås op for nye seje features!"
         }
       />
-      <Underoverskrift tekst={"Karakterer"} />
+      <br />
+      <h1 className="font-bold mx-[12.5%] text-[15px] md:text-2xl">Frisure</h1>
+      <p className="mx-[12.5%] text-[12px] md:text-[20px]">
+        Find din favorit og klik for at købe den
+      </p>
+
+      {/* SEKTION MED FRISURE */}
       <section className="flex justify-between mx-[12.5%]">
-        {" "}
-        {/* Themable klasse bruges til at ændre farverne på baggrunden af denne sektion baseret på det valgte tema */}
-        <aside className="w-[60px] h-[120px] border rounded bg-[#FCF0F0] border-[#d13338]"></aside>{" "}
-        {/* Hver karakter er repræsenteret af et aside-element med en bredde på 100px, en højde på 132px, en kant og afrundede hjørner */}
-        <aside className="w-[60px] h-[120px] border rounded bg-[#FFF8E7] border-[#ffc538]"></aside>{" "}
-        {/* Hver karakter er repræsenteret af et aside-element med en bredde på 100px, en højde på 132px, en kant og afrundede hjørner */}
-        <aside className="w-[60px] h-[120px] border rounded bg-[#E7FEF0] border-[#17be55]"></aside>{" "}
-        {/* Hver karakter er repræsenteret af et aside-element med en bredde på 100px, en højde på 132px, en kant og afrundede hjørner */}
-        <aside className="w-[60px] h-[120px] border rounded bg-[#E8F5FF] border-[#0d22e8]"></aside>{" "}
-        {/* Hver karakter er repræsenteret af et aside-element med en bredde på 100px, en højde på 132px, en kant og afrundede hjørner */}
+        <img src={pil} alt="Pil til venstre" className="rotate-180" />{" "}
+        {/* PIL TIL VENSTRE I FRSURE */}
+        {/* VÆLG FRISUTRE ET */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full"></div>
+
+            {/* Frisure */}
+            <img
+              src={frisureEt}
+              alt="Frisure Et"
+              className="absolute top-[75%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[110%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG FRISUTRE TO */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full"></div>
+
+            {/* Frisuren */}
+            <img
+              src={frisureTo}
+              alt="Frisure To"
+              className="absolute top-[63%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[110%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG FRISURE TRE */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full"></div>
+
+            {/* Frisuren */}
+            <img
+              src={frisureTre}
+              alt="Frisure Tre"
+              className="absolute top-[55%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[110%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        <img src={pil} alt="Pil til højre" /> {/* PIL TIL HØJRE I FRSURE */}
       </section>
 
-      <Underoverskrift tekst={"Tema"} />
+      <br />
+      <h1 className="font-bold mx-[12.5%] text-[15px] md:text-2xl">Ansigtsudtryk</h1>
+      <p className="mx-[12.5%] text-[12px] md:text-[20px]">
+        Find din favorit og klik for at købe den
+      </p>
+
+        {/* SEKTION MED ANSIGT */}
       <section className="flex justify-between mx-[12.5%]">
-        <aside
-          onClick={() => setTema("roed")}
-          className="w-[55px] h-[55px] border rounded bg-[#FCF0F0] border-[#d13338]"
-        ></aside>{" "}
+        <img src={pil} alt="Pil til venstre" className="rotate-180" />{" "}
+        {/* PIL TIL VENSTRE I ANSIGT */}
+        {/* VÆLG ANSIGT ET */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[80%] bg-[#d9d9d9] rounded-bl-[55%_90%] rounded-br-[55%_90%] rotate-180"></div>
+
+            {/* Ansigtet */}
+            <img
+              src={ansigtEt}
+              alt="Ansigt et"
+              className="absolute top-[67%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[50%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG ANSIGT TO */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[80%] bg-[#d9d9d9] rounded-bl-[55%_90%] rounded-br-[55%_90%] rotate-180"></div>
+
+            {/* Ansigtet */}
+            <img
+              src={ansigtTo}
+              alt="Ansigt to"
+              className="absolute top-[67%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[50%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG ANSIGT TRE */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i bunden */}
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[80%] bg-[#d9d9d9] rounded-bl-[55%_90%] rounded-br-[55%_90%] rotate-180"></div>
+
+            {/* Ansigtet */}
+            <img
+              src={ansigtTre}
+              alt="Ansigt tre"
+              className="absolute top-[67%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[50%]"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        <img src={pil} alt="Pil til højre" /> {/* PIL TIL HØJRE I ANSIGT */}
+      </section>
+
+      <br />
+      <h1 className="font-bold mx-[12.5%] text-[15px] md:text-2xl">Sko</h1>
+      <p className="mx-[12.5%] text-[12px] md:text-[20px]">
+        Find din favorit og klik for at købe den
+      </p>
+
+      {/* SEKTION MED SKO */}
+      <section className="flex justify-between mx-[12.5%]">
+        <img src={pil} alt="Pil til venstre" className="rotate-180" />{" "}
+        {/* PIL TIL VENSTRE I SKO */}
+        {/* VÆLG SKO ET */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i toppen */}
+            <div className="absolute top-0 rotate-180 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full z-50"></div>
+
+            {/* Sko */}
+            <img
+              src={skoEt}
+              alt="Sko Et"
+              className="absolute top-[60%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[70%] z-40"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG SKO TO */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i toppen */}
+            <div className="absolute top-0 rotate-180 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full z-50"></div>
+
+            {/* Sko */}
+            <img
+              src={skoTo}
+              alt="Sko To"
+              className="absolute top-[60%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[70%] z-40"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        {/* VÆLG SKO TRE */}
+        <aside className="flex flex-col items-center">
+          <div className="w-[75px] h-[75px] border rounded md:w-[100px] md:h-[100px] relative overflow-hidden">
+            {/* Halvcirkel i toppen */}
+            <div className="absolute top-0 rotate-180 left-1/2 transform -translate-x-1/2 w-[80%] h-[50%] bg-[#d9d9d9] rounded-tl-full rounded-tr-full z-50"></div>
+
+            {/* Sko */}
+            <img
+              src={skoTre}
+              alt="Sko Et"
+              className="absolute top-[60%] left-1/2 object-contain transform -translate-x-1/2 -translate-y-1/2 md:w-[70%] z-40"
+            />
+          </div>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
+        <img src={pil} alt="Pil til højre" /> {/* PIL TIL HØJRE I SKO */}
+      </section>
+
+      <br />
+
+        {/* SEKTION MED TEMAER */}
+      <h1 className="font-bold mx-[12.5%] text-[15px] md:text-2xl">Temaer</h1>
+      <p className="mx-[12.5%] text-[12px] md:text-[20px]">
+        Find din favorit og klik for at købe den
+      </p>
+      <section className="flex justify-between mx-[12.5%]">
+        <img src={pil} alt="Pil til venstre" className="rotate-180" />
+        <aside>
+          <aside
+            onClick={() => setTema("roed")}
+            className="w-[75px] h-[75px] border rounded bg-[#FCF0F0] border-[#d13338] md:w-[100px] md:h-[100px]"
+          ></aside>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
         {/* Klik på denne sektion ændrer temaet til rødt */}
-        <aside
-          onClick={() => setTema("gul")}
-          className="w-[55px] h-[55px] border rounded bg-[#FFF8E7] border-[#ffc538]"
-        ></aside>{" "}
+        <aside>
+          <aside
+            onClick={() => setTema("gul")}
+            className="w-[75px] h-[75px] border rounded bg-[#FFF8E7] border-[#ffc538] md:w-[100px] md:h-[100px]"
+          ></aside>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
         {/* Klik på denne sektion ændrer temaet til gult */}
-        <aside
-          onClick={() => setTema("groen")}
-          className="w-[55px] h-[55px] border rounded bg-[#E7FEF0] border-[#17be55]"
-        ></aside>{" "}
+        <aside>
+          <aside
+            onClick={() => setTema("groen")}
+            className="w-[75px] h-[75px] border rounded bg-[#E7FEF0] border-[#17be55] md:w-[100px] md:h-[100px]"
+          ></aside>
+          <p className="text-center text-[10px] md:text-[15px]">100 point</p>
+        </aside>
         {/* Klik på denne sektion ændrer temaet til grønt */}
-        <aside
-          onClick={() => setTema("blaa")}
-          className="w-[55px] h-[55px] border rounded bg-[#E8F5FF] border-[#0d22e8]"
-        ></aside>{" "}
-        {/* Klik på denne sektion ændrer temaet til blåt */}
+        <img src={pil} alt="Pil til højre" />
       </section>
     </>
   );
